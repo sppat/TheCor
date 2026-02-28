83d6d114381b13bef01026c487aebaeb9a3c0ee8
Spyros Paterakis <s.paterakis@outlook.com.gr>
2026-02-28 18:53:43 +0200

    Add constant colors

--- TheCor.UI/App.js
+++ TheCor.UI/App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import color from './constants/color';
 
 export default function App() {
 	return (
-		<View>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<SafeAreaProvider>
+			<SafeAreaView style={styles.container}>
+				<Text style={styles.text}>Open up App.js to start working on your app!</Text>
+				<StatusBar style='light' />
+			</SafeAreaView>
+		</SafeAreaProvider>
 	);
 }
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: color.primaryBackground,
+	},
+	text: {
+		color: color.primaryText,
+	},
+});
---
+++ TheCor.UI/constants/color.js
@@ -0,0 +1,9 @@
+const color = {
+  primaryBackground: '#091223',
+  secondaryBackground: '#111827',
+  borderColor: '#1f2937',
+  primaryText: '#ffffff',
+  secondaryText: '#9ca3af',
+};
+
+export default color;
