fd7dafc464097927954725b08b9fa7419b76f58a
Spyros Paterakis <s.paterakis@outlook.com.gr>
2026-02-28 19:28:50 +0200

    Add boilerplate code for bottom tab navigator

--- TheCor.UI/App.js
+++ TheCor.UI/App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { createStaticNavigation, NavigationContainer } from '@react-navigation/native';
 import color from './constants/color';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 	return (
-		<SafeAreaProvider>
-			<SafeAreaView style={styles.container}>
-				<Text style={styles.text}>Open up App.js to start working on your app!</Text>
-				<StatusBar style='light' />
-			</SafeAreaView>
-		</SafeAreaProvider>
+		<>
+			<SafeAreaProvider>
+				<SafeAreaView style={styles.container}>
+					<NavigationContainer>
+						<Tab.Navigator>
+							<Tab.Screen name='Home' options={{ headerShown: false }} component={() => <Text style={{ flex: 1, backgroundColor: 'red' }}>Home</Text>} />
+							<Tab.Screen name='Settings' options={{ headerShown: false }} component={() => <Text style={{ flex: 1, backgroundColor: 'white' }}>Settings</Text>} />
+						</Tab.Navigator>
+					</NavigationContainer>
+				</SafeAreaView>
+			</SafeAreaProvider>
+			<StatusBar style='light' />
+		</>
 	);
 }
 
