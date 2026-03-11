012900adaea3d32a52ee9561d89e123849d30230
Spyros Paterakis <s.paterakis@outlook.com.gr>
2026-03-11 13:36:57 +0200

    Apply code format

--- TheCor.UI/components/EmptyCard.js
+++ TheCor.UI/components/EmptyCard.js
@@ -3,28 +3,28 @@ import { Ionicons } from '@expo/vector-icons';
 import color from '../constants/color';
 
 export default function EmptyCard({ message, image }) {
-  return (
-    <View style={styles.container}>
-      <Ionicons name={image} size={50} color={color.emptyIconBackground} />
-      <Text style={styles.text}>{message}</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Ionicons name={image} size={50} color={color.emptyIconBackground} />
+			<Text style={styles.text}>{message}</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: color.secondaryBackground,
-    opacity: 0.8,
-    borderRadius: 12,
-    borderWidth: 0.5,
-    padding: 20,
-    borderColor: color.borderColor,
-    marginTop: 10,
-  },
-  text: {
-    color: color.secondaryText,
-    marginTop: 10,
-  },
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: color.secondaryBackground,
+		opacity: 0.8,
+		borderRadius: 12,
+		borderWidth: 0.5,
+		padding: 20,
+		borderColor: color.borderColor,
+		marginTop: 10,
+	},
+	text: {
+		color: color.secondaryText,
+		marginTop: 10,
+	},
 });
